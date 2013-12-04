ae286683641fda094090c21dd4f7a75aca5039c7
Ethan Snyder <ethan.snyder@usafa.edu>
2013-12-04 11:27:40 -0700

    adding additional code for second LED

--- main.c
+++ main.c
@@ -3,22 +3,48 @@
 int main(void)
 {
   WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+while(1){
   ADC10CTL0 = ADC10SHT_3 + ADC10ON + ADC10IE; // ADC10ON, interrupt enabled
   ADC10CTL1 = INCH_4;                       // input A4
+
   ADC10AE0 |= BIT4;                         // PA.1 ADC option select
+
   ADC10CTL1 |= ADC10SSEL1|ADC10SSEL0;                // Select SMCLK
+
   P1DIR |= 0x01;                            // Set P1.0 to output direction
 
-  for (;;)
-  {
+
     ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
     __bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
     if (ADC10MEM < 0x1FF)
       P1OUT &= ~0x01;                       // Clear P1.0 LED off
     else
       P1OUT |= 0x01;                        // Set P1.0 LED on
-  }
 
+
+
+
+    ADC10CTL1 = INCH_5;                       // input A4
+
+    ADC10AE0 |= BIT5;                         // PA.1 ADC option select
+
+    ADC10CTL1 |= ADC10SSEL1|ADC10SSEL0;                // Select SMCLK
+
+    P1DIR |= 0x06;                            // Set P1.0 to output direction
+
+
+
+      ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
+      __bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
+      if (ADC10MEM < 0x1FF)
+        P1OUT &= ~0x06;                       // Clear P1.0 LED off
+      else
+        P1OUT |= 0x06;                        // Set P1.0 LED on
+
+
+
+
+}
     return 0;
 }
 
