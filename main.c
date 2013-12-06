d0397fcf4e3067f17bc85ea2de379902b3552dc6
Ethan Snyder <ethan.snyder@usafa.edu>
2013-12-06 11:42:03 -0700

    working code, still need to adjust sensitivity for left sensor

--- main.c
+++ main.c
@@ -4,10 +4,14 @@ int main(void)
 {
   WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
 while(1){
-  ADC10CTL0 = ADC10SHT_3 + ADC10ON + ADC10IE; // ADC10ON, interrupt enabled
 
 
+	ADC10CTL0 = ADC10SHT_3 + ADC10ON + ADC10IE; // ADC10ON, interrupt enabled
 
+
+  ADC10CTL0 &= ~ENC;
+
+  ADC10CTL1 |= ADC10DIV0|ADC10DIV1|ADC10DIV2; // divide clock by 8
 
   ADC10CTL1 = INCH_4;                       // input A4
 
@@ -15,12 +19,16 @@ while(1){
 
   ADC10CTL1 |= ADC10SSEL1|ADC10SSEL0;                // Select SMCLK
 
+
+
+
+
   P1DIR |= 0x01;                            // Set P1.0 to output direction
 
 
     ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
     __bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
-    if (ADC10MEM < 0x1FF)
+    if (ADC10MEM < 0x2FF)
     {
       P1OUT &= ~0x01;
     } // Clear P1.0 LED off
@@ -31,21 +39,25 @@ while(1){
 
 
     // start sensor 2 code
+    ADC10CTL0 &= ~ENC;
+
+
+
     ADC10CTL1 = INCH_5;                       // input A4
 
       ADC10AE0 |= BIT5;                         // PA.1 ADC option select
 
-      P1DIR |= 0x06;
+      P1DIR |= 0x40;
 
       ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
           __bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
-          if (ADC10MEM < 0x1FF)
+          if (ADC10MEM < 0x29F)
           {
-            P1OUT &= ~0x06;
+            P1OUT &= ~0x40;
           } // Clear P1.0 LED off
           else
           {
-            P1OUT |= 0x06;                        // Set P1.0 LED on
+            P1OUT |= 0x40;                        // Set P1.0 LED on
           }
 
 
