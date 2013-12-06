702ab597d5a5116a8b1b728c374cbe80d5cbb719
Ethan Snyder <ethan.snyder@usafa.edu>
2013-12-06 10:43:49 -0700

    debugging, going back to original one sensor config

--- main.c
+++ main.c
@@ -23,27 +23,6 @@ while(1){
 
 
 
-
-    ADC10CTL1 = INCH_5;                       // input A4
-
-    ADC10AE0 |= BIT5;                         // PA.1 ADC option select
-
-    ADC10CTL1 |= ADC10SSEL1|ADC10SSEL0;                // Select SMCLK
-
-    P1DIR |= 0x06;                            // Set P1.0 to output direction
-
-
-
-      ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
-      __bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
-      if (ADC10MEM < 0x1FF)
-        P1OUT &= ~0x06;                       // Clear P1.0 LED off
-      else
-        P1OUT |= 0x06;                        // Set P1.0 LED on
-
-
-
-
 }
     return 0;
 }
